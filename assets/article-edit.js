d708b58d3d7e51e848262cf66bcc7608aaa7e86d
Oliver Lago <oliver.oli.lago@gmail.com>
2025-09-08 20:18:53 +0200

    Fix js

--- assets/article-edit.js
+++ assets/article-edit.js
@@ -1,21 +1,17 @@
-
-window.addEventListener('load', () => {
-    document.getElementById('back').addEventListener('click', (event) => {
-        window.location.href='../articles';
-    });
-    const saveButton = document.getElementById('save');
-    document.getElementById('name').addEventListener('input', (event) => {
-        if (event.target.value.length > 0)
-        {
-            saveButton.disabled = false;
-            saveButton.classList.remove('dialog-disabled');
-            saveButton.classList.add('green');
-        }
-        else
-        {
-            saveButton.disabled = true;
-            saveButton.classList.add('dialog-disabled');
-            saveButton.classList.remove('green');
-        }
-    });
+window.addEventListener("load", () => {
+  document.getElementById("back").addEventListener("click", (event) => {
+    window.location.href = "../articles";
+  });
+  const saveButton = document.getElementById("save");
+  document.getElementById("name").addEventListener("input", (event) => {
+    if (event.target.value.length > 0) {
+      saveButton.disabled = false;
+      saveButton.classList.remove("dialog-disabled");
+      saveButton.classList.add("green");
+    } else {
+      saveButton.disabled = true;
+      saveButton.classList.add("dialog-disabled");
+      saveButton.classList.remove("green");
+    }
+  });
 });
--- assets/article.js
+++ assets/article.js
@@ -1,57 +1,52 @@
-
-async function likeArticle(event)
-{
-    try
-    {
-        const id = window.location.pathname.split('/')[4];
-        const url = window.location.origin + `/~38613013/cms/like/${id}`;
-        const response = await fetch(url, { method: 'POST', credentials: 'include' });
-        if (!response.ok)
-        {
-            throw new Error(`${response.status} ${response.statusText}`);
-        }
-        event.target.classList.add('liked');
-        event.target.disabled = true;
-        event.target.innerText = 'Liked';
+async function likeArticle(event) {
+  try {
+    const id = window.location.pathname.split("/")[2];
+    const url = window.location.origin + `/like/${id}`;
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "include",
+    });
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
     }
-    catch (e)
-    {
-        //console.log(e);
-        event.target.disabled = true;
-    }
+    event.target.classList.add("liked");
+    event.target.disabled = true;
+    event.target.innerText = "Liked";
+  } catch (e) {
+    //console.log(e);
+    event.target.disabled = true;
+  }
 }
 
-async function dislikeArticle(event)
-{
-    try
-    {
-        const id = window.location.pathname.split('/')[4];
-        const url = window.location.origin + `/~38613013/cms/dislike/${id}`;
-        const response = await fetch(url, { method: 'POST', credentials: 'include' });
-        if (!response.ok)
-        {
-            throw new Error(`${response.status} ${response.statusText}`);
-        }
-        event.target.classList.add('disliked');
-        event.target.disabled = true;
-        event.target.innerText = 'Disliked';
+async function dislikeArticle(event) {
+  try {
+    const id = window.location.pathname.split("/")[2];
+    const url = window.location.origin + `/dislike/${id}`;
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "include",
+    });
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
     }
-    catch (e)
-    {
-        //console.log(e);
-        event.target.disabled = true;
-    }
+    event.target.classList.add("disliked");
+    event.target.disabled = true;
+    event.target.innerText = "Disliked";
+  } catch (e) {
+    //console.log(e);
+    event.target.disabled = true;
+  }
 }
 
-window.addEventListener('load', () => {
-    document.getElementById('back').addEventListener('click', (event) => {
-        window.location.href='../articles';
-    });
-    document.getElementById('edit-article').addEventListener('click', (event) => {
-        const id = window.location.pathname.split('/')[4];
-        window.location.href=`../article-edit/${id}`;
-    });
+window.addEventListener("load", () => {
+  document.getElementById("back").addEventListener("click", (event) => {
+    window.location.href = "../articles";
+  });
+  document.getElementById("edit-article").addEventListener("click", (event) => {
+    const id = window.location.pathname.split("/")[2];
+    window.location.href = `../article-edit/${id}`;
+  });
 
-    document.getElementById('like').addEventListener('click', likeArticle);
-    document.getElementById('dislike').addEventListener('click', dislikeArticle);
+  document.getElementById("like").addEventListener("click", likeArticle);
+  document.getElementById("dislike").addEventListener("click", dislikeArticle);
 });
